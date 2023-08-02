1a9efc3ac1accfdb2eaca3bd4e8ce20563d75a73
Kingsley Robert-Ezenta <krobertezenta@gmail.com>
2023-08-02 10:37:02 -0600

    Form Validation with class components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+import TodoCategory from './TodoCategory';
+
+import logo from './images/logo.svg';
+
+import './App.css';
+
+const App = () => {
+
+  
+
+  return (
+    <>
+      <Header className='top-header' id='header' title='This is the header'>
+        <h1>Kingsley's TodoList 🚀🐨</h1>
+        <img alt='' src={logo} width={55} height={55} />
+      </Header>
+
+      <TodoCategory title='Morning' />
+      <TodoCategory title='Afternoon' />
+      <TodoCategory title='Night' />
+      
+      <Footer />
+    </>
+  );
+}
+
+export default App;
